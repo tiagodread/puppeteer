9afbbeac8b561aa1259782367d112ea9f2ed3540
Tiago Góes <tiago@geru.com.br>
2019-03-27 14:59:34 -0300

    fix test

--- home.test.js
+++ home.test.js
@@ -4,27 +4,28 @@ let browser;
 let page;
 let urlBase = 'https://www.geru.com.br/emprestimos';
 
+
 beforeAll(async () => {
     browser = await puppeteer.launch({headless: true});
     page = await browser.newPage();
     page.setViewport({width: 1366, height: 768});
 });
 
-test('take an screenshot of homepage', async () => {
+it('take an screenshot of homepage', async () => {
     await page.goto(urlBase);
     await page.screenshot({path: 'screenshots/home.png', fullPage: true});
-});
-
+}, 15000);
 
 it('navigate to login from homepage', async ()=>{
     await page.goto(urlBase);
     await page.waitForSelector('#header-myaccount-button-personal');
     await page.click('#header-myaccount-button-personal');
+    const email = await page.waitForSelector('input[name="email"]');
+    const password = await page.waitForSelector('input[name="password"]');
+    expect(email).toBeTruthy();
+    expect(password).toBeTruthy();
 
-    await expect(page.waitForSelector('email').toBeTruthy());
-
-
-});
+}, 15000);
 
 afterAll(async () => {
     browser.close();
